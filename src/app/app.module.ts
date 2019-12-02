048229a4f6714480711670b42670b3cbaf290c23
unknown <lakha117@uwindsor.ca>
2019-12-01 23:00:16 -0500

    feat: Add base code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { SearchPageComponent } from './components/search-page/search-page.component';
+// import { WavesModule, InputsModule, ButtonsModule } from 'ng-uikit-pro-standard';
+import { WavesModule, InputsModule, ButtonsModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+import { NgMatSearchBarModule } from 'ng-mat-search-bar';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WavesModule,
+    InputsModule,
+    ButtonsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
